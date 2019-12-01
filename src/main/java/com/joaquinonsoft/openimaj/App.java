c768f07ffa02103472776e532846a475e50c832f
Joaquin Garzon Pena <jgarzonpena@opentext.com>
2019-12-01 23:13:37 +0100

    Chapter 03

--- src/main/java/com/joaquinonsoft/openimaj/App.java
+++ src/main/java/com/joaquinonsoft/openimaj/App.java
@@ -1,31 +1,75 @@
 package com.joaquinonsoft.openimaj;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+
 import org.openimaj.image.DisplayUtilities;
+import org.openimaj.image.ImageUtilities;
 import org.openimaj.image.MBFImage;
 import org.openimaj.image.colour.ColourSpace;
-import org.openimaj.image.colour.RGBColour;
-import org.openimaj.image.processing.convolution.FGaussianConvolve;
+import org.openimaj.image.connectedcomponent.GreyscaleConnectedComponentLabeler;
+import org.openimaj.image.pixel.ConnectedComponent;
 import org.openimaj.image.typography.hershey.HersheyFont;
+import org.openimaj.ml.clustering.FloatCentroidsResult;
+import org.openimaj.ml.clustering.assignment.HardAssigner;
+import org.openimaj.ml.clustering.kmeans.FloatKMeans;
 
 /**
- * OpenIMAJ Hello world!
- *
+ * Chapter 3. Introduction to clustering, segmentation and connected components
  */
 public class App {
-    public static void main( String[] args ) {
-    	//Create an image
-        MBFImage image = new MBFImage(320,70, ColourSpace.RGB);
+	public static void main( String[] args ) {
+		//Create an image
+		MBFImage input = null;
+		try {
+			input = ImageUtilities.readMBF(new URL("http://static.openimaj.org/media/tutorial/sinaface.jpg"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        //Fill the image with white
-        image.fill(RGBColour.WHITE);
-        		        
-        //Render some test into the image
-        image.drawText("Hello World", 10, 60, HersheyFont.CURSIVE, 50, RGBColour.BLACK);
+		if(input != null) {    
+			System.out.println(input.colourSpace);
 
-        //Apply a Gaussian blur
-        image.processInplace(new FGaussianConvolve(2f));
-        
-        //Display the image
-        DisplayUtilities.display(image);
-    }
+			input = ColourSpace.convert(input, ColourSpace.CIE_Lab);
+
+			System.out.println(input.colourSpace);
+
+			FloatKMeans cluster = FloatKMeans.createExact(4);
+
+			float[][] imageData = input.getPixelVectorNative(new float[input.getWidth() * input.getHeight()][3]);
+
+			FloatCentroidsResult result = cluster.cluster(imageData);
+
+			float[][] centroids = result.centroids;
+			for (float[] fs : centroids) {
+				System.out.println(Arrays.toString(fs));
+			}
+
+			HardAssigner<float[],?,?> assigner = result.defaultHardAssigner();
+			for (int y=0; y<input.getHeight(); y++) {
+				for (int x=0; x<input.getWidth(); x++) {
+					float[] pixel = input.getPixelNative(x, y);
+					int centroid = assigner.assign(pixel);
+					input.setPixelNative(x, y, centroids[centroid]);
+				}
+			}	
+
+			input = ColourSpace.convert(input, ColourSpace.RGB);
+			DisplayUtilities.display(input);
+
+			GreyscaleConnectedComponentLabeler labeler = new GreyscaleConnectedComponentLabeler();
+			List<ConnectedComponent> components = labeler.findComponents(input.flatten());
+
+			int i = 0;
+			for (ConnectedComponent comp : components) {
+				if (comp.calculateArea() < 50)
+					continue;
+				input.drawText("Point:" + (i++), comp.calculateCentroidPixel(), HersheyFont.TIMES_MEDIUM, 20);
+			}
+
+			DisplayUtilities.display(input);
+		}
+	}
 }
